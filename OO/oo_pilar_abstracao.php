3781a7c7af87993874822cf79818b2a033b8d8d6
Jefferson Teles de Souza <telesjefferson2@gmail.com>
2019-06-13 18:04:41 -0300

    getters e setters overloading e sobrecarga

--- OO/oo_pilar_abstracao.php
+++ OO/oo_pilar_abstracao.php
@@ -6,8 +6,18 @@
     public $nome = null;
     public $telefone = null;
     public $numFilhos = null;
+    public $cargo = null;
+    public $salario = Null;
 
-    // getters & setters
+    // getters & setters (overloading / sobrecarga)
+    function __set($name, $value) {
+        $this->$name = $value;
+    }
+
+    function __get($name) {
+        return $this->$name;
+    }
+    /*
     function setNome($nome) {
         $this->nome = $nome;
     }
@@ -23,10 +33,11 @@
     function getNumFilhos() {
         return $this->numFilhos;
     }
+    */
     
     // métodos
     function resumirCadFunc() {
-        return "$this->nome possui $this->numFilhos filho(s)";
+        return $this->__get('nome') . " possui " . $this->__get('numFilhos') . " filho(s)";
     }
 
     function modificarNumFilhos($numFilhos) {
@@ -35,14 +46,19 @@
   }
 
   $y = new Funcionario();
-  $y->setNome('Jose');
-  $y->setNumFilhos(2);
-  // echo $y->resumirCadFunc();
-  echo $y->getNome() .  ' possui ' . $y->getNumFilhos() . ' filho(s)';
+  $y->__set('nome', 'José');
+  $y->__set('numFilhos', 2);
+  $y->__set('cargo', 'Design');
+  $y->__set('salario', 1500);
+  echo $y->resumirCadFunc();
+//   echo $y->__get('nome') .  ' possui ' . $y->__get('numFilhos') . ' filho(s), seu cargo é de ' . $y->__get('cargo'). ' é seu salario e de ' . $y->__get('salario') .'R$';
 
   echo '<br>';
   $x = new Funcionario();
-  $x->getNome('Maria');
-  $x->getNumFilhos(0);
-  echo $x->getNome() .  ' possui ' . $x->getNumFilhos() . ' filho(s)';
+  $x->__set('nome', 'Maria');
+  $x->__set('numFilhos', 0);
+  $x->__set('cargo', 'Programadora');
+  $x->__set('salario', 2100);
+  echo $x->resumirCadFunc();
+//   echo $x->__get('nome') .  ' possui ' . $x->__get('numFilhos') . ' filho(s), seu cargo é de ' . $x->__get('cargo'). ' é seu salario e de ' . $x->__get('salario') .'R$';
 ?>
