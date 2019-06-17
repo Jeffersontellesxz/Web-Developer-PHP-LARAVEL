89e2d759666701fed063052094f0b555a4683f0a
Jefferson Teles de Souza <telesjefferson2@gmail.com>
2019-06-17 11:07:34 -0300

    Trabalhando com bibliotecas, namespace, use, interface

---
+++ OO/atributos_e_metodos.php
@@ -0,0 +1,24 @@
+<?php
+    class Exemplo {
+        public static $atributo1 = 'Eu sou um atributo estatico';
+        public $atributo2 = 'Eu sou um atributo normal';
+
+        public static function metodo1() {
+            echo 'Eu sou um metodo estatico';
+        }
+
+        public function metodo2() {
+            echo 'Eu sou um metodo normal';
+        }
+    }
+
+    $x = new Exemplo();
+    /*
+    echo Exemplo::$atributo1;
+    echo '<br>';
+    Exemplo::metodo1();
+    */
+
+    # echo $x->atributo2; // Nao funciona;
+    
+?>
